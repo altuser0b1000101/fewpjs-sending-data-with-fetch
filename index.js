fab38f12f610f5e0552074a4654a34cc7adeba8c
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-05-26 23:13:11 -0400

    commit tree ba5bd0c6ed43c0f1c13996529466f38f8599a491

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-// Add your code here
+document.addEventListener("DOMContentLoaded", () => { 
+})   
+const configObj = {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+      name:  "Steve",
+      email: "steve@steve.com" 
+  })
+};
+
+function submitData(){
+    fetch("http://localhost:3000/users", configObj) 
+    .then(function(resp){
+        return resp.json();
+     })
+     .then(function(object){
+        console.log(object)
+    })
+    .catch(function(error){
+     alert("Unauthorized Access");
+     console.log(error.message);
+  })
+  
+}
+
+
+
+
+
+
+
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+
+
+//   let configObj = {
+//       method: "POST",
+//       headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     })
+//     .catch(function(error) {
+//       alert("Bad things! Ragnarők!");
+//       console.log(error.message);
+//     });
